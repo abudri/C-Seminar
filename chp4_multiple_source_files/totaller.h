3ed579d6ba503412045093d2e7047f277f235bcf
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-10-26 11:29:35 -0400

    update

---
+++ chp4_multiple_source_files/totaller.h
@@ -0,0 +1,6 @@
+/* a header file we use for function declarations.  we define (with body) these functions 
+wherever else we want in the code and order of appearance in the code won't matter, pg 147*/
+
+float add_with_tax(float f);  /* a Declaration. Fuction will be Defined with body in the
+ code that uses this header file.  This will be referenced in other files with #include "totaller.h" */
+ // assd
